61152afb80905429c07a4a5326bc59c79da2e7dc
roberth samuel simon <robertferdinandus360@gmail.com>
2019-10-27 16:05:09 +0700

    Add project files.

---
+++ ToDo_Roberth/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDo_Roberth.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int PublicationYear { get; set; }
+        public int Minimum_Type { get; set; }
+        public int Maximum_Type { get; set; }
+    }
+}
---
+++ ToDo_Roberth/Pages/Games/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ToDo_Roberth.Models;
+
+namespace ToDo_Roberth.Pages.Games
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ToDo_Roberth.Models.AppDbContext _context;
+
+        public IndexModel(ToDo_Roberth.Models.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Game> Game { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Game = await _context.Game.ToListAsync();
+        }
+    }
+}
